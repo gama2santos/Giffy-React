fe65b26396518a0588b49a74f619d15e4f8e0a0b
Gamadossantos <milan_total90@hotmail.com>
2021-08-23 16:24:06 -0500

    Updates

---
+++ precache-manifest.2dd40973edca84b711970b5fb189c77b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb24089db38be6b3af0a64075b50f99",
+    "url": "/Giffy-React/index.html"
+  },
+  {
+    "revision": "dcffa3ffa6089d58e1c2",
+    "url": "/Giffy-React/static/css/3.5eef0e01.chunk.css"
+  },
+  {
+    "revision": "20d41ee6ffe0dd0a6fe2",
+    "url": "/Giffy-React/static/css/main.d2926de2.chunk.css"
+  },
+  {
+    "revision": "a14b0b198884ca3b1eb2",
+    "url": "/Giffy-React/static/js/2.fe960cca.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Giffy-React/static/js/2.fe960cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcffa3ffa6089d58e1c2",
+    "url": "/Giffy-React/static/js/3.ac8886a7.chunk.js"
+  },
+  {
+    "revision": "23e862939f995f1f187e",
+    "url": "/Giffy-React/static/js/4.c20fb30e.chunk.js"
+  },
+  {
+    "revision": "d259e1ee5ee08a7b48ac",
+    "url": "/Giffy-React/static/js/5.5b600bde.chunk.js"
+  },
+  {
+    "revision": "20d41ee6ffe0dd0a6fe2",
+    "url": "/Giffy-React/static/js/main.b5b37be1.chunk.js"
+  },
+  {
+    "revision": "ceb02b7c61bacc893dc8",
+    "url": "/Giffy-React/static/js/runtime-main.4391d60e.js"
+  }
+]);
